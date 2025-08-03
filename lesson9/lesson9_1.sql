bcda518813e6a6b1559f890e82a9a4b2a64a63dd
noellechen <noelle1221.tw@gmail.com>
2025-08-03 04:02:16 +0000

    0803上午

--- lesson9/lesson9_1.sql
+++ lesson9/lesson9_1.sql
@@ -90,4 +90,48 @@ SELECT "name" AS 站名,date_part('year',"日期" ) AS 年份 ,count("name") AS 
 FROM "每日各站進出站人數" LEFT JOIN "台鐵車站資訊" ON "車站代碼" = "stationCode"
 WHERE "name" = '基隆'
 GROUP BY "name", "年份"
-ORDER BY "進站人數" DESC; 
+ORDER BY "進站人數" DESC;
+
+/*全台各站點2022年進站總人數大於500萬人的站點 */
+
+/*這是問MCP所得到的code*/
+SELECT
+    t."stationName" AS "車站名稱",
+    SUM(p."進站人數") AS "2022年進站總人數"
+FROM "每日各站進出站人數" p
+LEFT JOIN "台鐵車站資訊" t ON p."車站代碼" = t."stationCode"
+WHERE DATE_PART('year', p."日期") = 2022
+GROUP BY t."stationCode", t."stationName"
+HAVING SUM(p."進站人數") > 5000000
+ORDER BY SUM(p."進站人數") DESC;
+
+
+/*
+*基隆火車站2020,2021,2022,每年進站人數
+*/
+SELECT date_part('year', t1."日期") AS 年份, SUM(t1."進站人數") AS 進站總人數
+FROM "每日各站進出站人數" t1
+LEFT JOIN "台鐵車站資訊" t2 ON t1."車站代碼" = t2."stationCode"
+WHERE t2."name" = '基隆' AND date_part('year', t1."日期") IN (2020, 2021, 2022)
+GROUP BY 年份
+ORDER BY 年份;
+
+/*
+*基隆火車站,臺北火車站2020,2021,2022,每年進站人數
+*/
+SELECT t2."name" AS 站名, date_part('year', t1."日期") AS 年份, SUM(t1."進站人數") AS 進站總人數
+FROM "每日各站進出站人數" t1
+LEFT JOIN "台鐵車站資訊" t2 ON t1."車站代碼" = t2."stationCode"
+WHERE t2."name" IN ('基隆', '臺北') AND date_part('year', t1."日期") IN (2020, 2021, 2022)
+GROUP BY t2."name", 年份
+ORDER BY t2."name", 年份;
+/*
+*查詢 2022 年平均每日進站人數超過 2 萬人的站點
+*/
+SELECT t2."name" AS 站名, AVG(t1."進站人數") AS 平均每日進站人數
+FROM "每日各站進出站人數" t1
+LEFT JOIN "台鐵車站資訊" t2 ON t1."車站代碼" = t2."stationCode"
+WHERE t1."日期" BETWEEN '2022-01-01' AND '2022-12-31'
+GROUP BY t2."name"
+HAVING AVG(t1."進站人數") > 20000
+ORDER BY 平均每日進站人數 DESC;
